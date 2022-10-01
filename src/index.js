822ec867899dab5576e97864d0b510968d0ea1a9
rebaz36 <rebaz415@gmail.com>
2022-10-01 12:40:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,63 +1,9 @@
 import './style.css';
 import icon from './icon.png';
-import ToDolist from './todolist.js';
-import { modificarBox, modifyDescription, deleteToDo } from './modify.js';
+import { render, myToDolist } from './modify.js';
 // import complete from './delete.js';
 
-const myToDolist = new ToDolist();
-
 myToDolist.LoadToDoFromLocal();
-
-const render = () => {
-  document.querySelector('.ToDoList').innerHTML = '';
-  for (let index = 0; index < myToDolist.ToDo.length; index += 1) {
-    const toDo = myToDolist.ToDo[index];
-
-    const List = document.querySelector('.ToDoList');
-    const element = document.createElement('li');
-    element.classList.add('eachToDo');
-    const completed = document.createElement('input');
-    completed.type = 'checkbox';
-    const inputTask = document.createElement('input');
-    inputTask.type = 'text';
-    inputTask.classList.add('item-input');
-    completed.classList.add('completed');
-    inputTask.value = toDo.description;
-    element.append(completed, inputTask);
-    completed.setAttribute('id', toDo.id);
-    inputTask.disabled = false;
-    completed.checked = toDo.completed;
-    if (completed.checked) {
-      inputTask.style.textDecoration = 'line-through';
-    }
-
-    inputTask.addEventListener('change', () => {
-      modifyDescription(index, inputTask.value, myToDolist);
-    });
-
-    completed.addEventListener('click', () => {
-      modificarBox(index, completed.checked, myToDolist);
-
-      if (completed.checked) {
-        inputTask.style.textDecoration = 'line-through';
-      } else {
-        inputTask.style.textDecoration = 'none';
-      }
-    });
-
-    List.appendChild(element);
-
-    // Remove Button
-    const deleteButton = document.createElement('input-btn');
-    const idToDelete = deleteButton.id;
-
-    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-    deleteButton.classList.add('delButton');
-    deleteButton.onclick = deleteToDo(idToDelete, myToDolist, render);
-    deleteButton.id = toDo.id;
-    element.appendChild(deleteButton);
-  }
-};
 
 render();
 // Controller
