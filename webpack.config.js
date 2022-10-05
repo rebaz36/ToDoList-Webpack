73bab4a4fa754e100209d13d6c82f606331003d1
rebaz36 <rebaz415@gmail.com>
2022-10-05 23:38:29 +0300

    fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,31 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-  devServer: {
-    static: './dist',
+  optimization: {
+    runtimeChunk: 'single',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-  optimization: {
-    runtimeChunk: 'single',
-  },
 };
